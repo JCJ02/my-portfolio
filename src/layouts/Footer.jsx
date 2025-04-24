01b0bda38fb26560e502d842e62564d938973b7b
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-04-24 08:41:00 +0800

    feat: make the year in the footer dynamically

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,12 +1,14 @@
 import { cn } from "@/lib/utils";
+import { yearToday } from "@/utilities/yearToday";
 import React from "react";
 
 const Footer = ({ className }) => {
+    const year = yearToday();
     return (
         <>
             <footer className={cn("bg-[#76ABAE] bottom-0 flex flex-col items-center w-full py-[24px]", className)}>
                 <p className="font-poppins font-semibold text-sm md:text-md lg:text-lg text-[#EEEEEE]">Developer: John Carlo P. Jacobe</p>
-                <p className="font-poppins text-xs text-[#EEEEEE]">Copyright &copy; 2024 | All Rights Reserved.</p>
+                <p className="font-poppins text-xs text-[#EEEEEE]">Copyright &copy; {year} | All Rights Reserved.</p>
             </footer>
         </>
     )
