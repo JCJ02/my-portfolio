6c878e5a747de9280cfcb19db40f33269d0ffacb
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-05-19 07:07:21 +0800

    fix: make it responsive

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -25,7 +25,8 @@ const Skills = () => {
     return (
         <>
             <div className="bg-[#EEEEEE] dark:bg-[#222831] flex justify-center items-center transition-colors duration-200 min-h-screen w-full" id="skills">
-                <div className="flex flex-col justify-between items-center gap-5 m-auto py-5 max-w-[1280px]">
+
+                <div className="flex flex-col justify-between items-center gap-5 px-5 md:px-16 2xl:px-0 py-6 m-auto w-[1280px]">
 
                     <div className="flex flex-col justify-start items-start lg:items-center gap-2 w-full">
                         <h1 className="font-poppins font-bold text-3xl 2xl:text-5xl text-[#222831] dark:text-[#EEEEEE] self-start lg:self-center">Skills</h1>
@@ -264,6 +265,7 @@ const Skills = () => {
                     </div>
 
                 </div>
+
             </div>
         </>
     )
