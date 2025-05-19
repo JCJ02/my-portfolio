e93834d074bbbf5095d0d086984a5d0b783410ab
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-05-20 01:11:15 +0800

    feat: add array inside the object for readability

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,15 +1,60 @@
 import projectOne from "../assets/images/POWERWALK-web-app.png";
 import projectTwo from "../assets/images/lws-invoice-app.png";
+import { techIcons } from "@/utilities/techIcons";
 
 export const ProjectsData = [
     {
         id: 1,
         projectName: "POWERWALK Web Application",
+        projectDescription: "",
         projectImage: projectOne,
+        keyFeature: "",
+        techStack: {
+            frontend: [
+                techIcons.languages[3],
+                techIcons.frontend[0],
+                techIcons.frontend[2]
+            ],
+            backend: [
+                techIcons.languages[3],
+                techIcons.backend[0],
+                techIcons.backend[1]
+            ],
+            database: [
+                techIcons.database[0]
+            ]
+        },
+        urls: {
+            frontendUrl: "",
+            backendUrl: "",
+            demoUrl: ""
+        }
     },
     {
         id: 2,
         projectName: "LWS Invoice Web Application",
+        projectDescription: "",
         projectImage: projectTwo,
+        keyFeatures: "",
+        techStack: {
+            frontend: [
+                techIcons.languages[3],
+                techIcons.frontend[1],
+                techIcons.frontend[2]
+            ],
+            backend: [
+                techIcons.languages[3],
+                techIcons.backend[0],
+                techIcons.backend[1]
+            ],
+            database: [
+                techIcons.database[1]
+            ]
+        },
+        urls: {
+            frontendUrl: "",
+            backendUrl: "",
+            demoUrl: ""
+        }
     }
 ]
